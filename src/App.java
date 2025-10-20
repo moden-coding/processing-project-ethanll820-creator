9ba30be8ac4642aec20a4dae52251242d0c53e52
Ethan <ethanll820@gmail.com>
2025-10-20 11:47:42 -0400

    done

--- src/App.java
+++ src/App.java
@@ -1,8 +1,12 @@
 import processing.core.*;
 
 public class App extends PApplet{
+     int rectx = 325;
+        int recty = 585;
+        int rectspeed = 10;
     public static void main(String[] args)  {
         PApplet.main("App");
+        
     }
 
     public void setup(){
@@ -14,8 +18,18 @@ public class App extends PApplet{
         size(800, 600);
     }
 
-    public void draw(){
-       
+    public void draw() {
+    background(100, 100, 200);
+    rect(rectx, recty, 150, 25);
+}
 
+
+    
+    public void keyPressed(){
+        if (keyCode == RIGHT){
+            rectx += rectspeed;
+        } if(keyCode == LEFT){
+            rectx-= rectspeed;
+        }
     }
 }
